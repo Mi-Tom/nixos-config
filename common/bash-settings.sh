1f48d7899ade9f000ec9d43901113753bbf9f636
M. Tomanec <mtomanec7@gmail.com>
2026-01-04 17:29:32 +0100

    uprava nastaveni terminalu

--- common/bash-settings.sh
+++ common/bash-settings.sh
@@ -9,12 +9,8 @@ Cyan='\033[1;36m'
 Yellow='\033[1;33m'
 NC='\033[0m'
 
-if [ -f ~/.nix-profile/share/git/contrib/completion/git-prompt.sh ]; then
-  source ~/.nix-profile/share/git/contrib/completion/git-prompt.sh
-elif [ -f /run/current-system/sw/share/git/contrib/completion/git-prompt.sh ]; then
-  source /run/current-system/sw/share/git/contrib/completion/git-prompt.sh
-elif [ -f /usr/share/git/completion/git-prompt.sh ]; then
-  source /usr/share/git/completion/git-prompt.sh
+if [ -f /run/current-system/sw/share/git/contrib/completion/git-prompt.sh ]; then
+    source /run/current-system/sw/share/git/contrib/completion/git-prompt.sh
 fi
 
 export GIT_PS1_SHOWDIRTYSTATE=1
