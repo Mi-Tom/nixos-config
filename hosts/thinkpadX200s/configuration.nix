9b18a20c2b338b0243d55f062178e676339b8a1d
M. Tomanec <mtomanec7@gmail.com>
2026-01-04 17:14:54 +0100

    pridano nastaveni power managgement pro thinkpad podle gemini

--- hosts/thinkpadX200s/configuration.nix
+++ hosts/thinkpadX200s/configuration.nix
@@ -15,12 +15,33 @@
 
   environment.systemPackages = with pkgs; [
     firefox
+    acpi       # Rychlý příkaz 'acpi -b' pro zjištění procent a času
+    powertop   # Spusť 'sudo powertop', přepni se na záložku 'Overview' a uvidíš aktuální odběr ve Wattech (W)
+    lm_sensors # Příkaz 'sensors' pro sledování teploty, aby větrák zbytečně nežral proud
   ];
   programs.firefox.enable = true;
   
   powerManagement.enable = true;
   services.power-profiles-daemon.enable = false;
-  services.tlp.enable = true;
+  powerManagement.powertop.enable = true; /*teoreticky ma vypnout vse co zrovna nepotrebuji*/
+  services.tlp = {
+  enable = true;
+  settings = {
+    # Na baterii chceme "powersave", na kabelu "performance".
+    CPU_SCALING_GOVERNOR_ON_AC = "performance";
+    CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+    # Nastaví nižší frekvenci grafiky při chodu na baterii.
+    INTEL_GPU_MIN_FREQ_ON_AC = 0;
+    INTEL_GPU_MAX_FREQ_ON_AC = 800;
+    INTEL_GPU_MIN_FREQ_ON_BAT = 0;
+    INTEL_GPU_MAX_FREQ_ON_BAT = 400;
+
+    # Uspání zvukové karty po 1 sekundě nečinnosti.
+    SOUND_POWER_SAVE_ON_BAT = 1;
+    USB_AUTOSUSPEND = 1;
+  };
+};
 
   zramSwap.enable = true;
   zramSwap.algorithm = "zstd";
